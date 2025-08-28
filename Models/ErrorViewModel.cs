4f131ae39a59a1eb8dfe08e1c2da55337141fdba
Hugo Huapaya <leonardo_2903@outlook.com>
2025-08-28 14:41:36 -0500

    Inicio de Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_programacion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
